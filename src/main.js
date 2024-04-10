a7b80148efad07435a9e43071ae8afc36c0cb37e
Thayllan Felipe Monteiro Siqueira <thayllan.siqueira@cleversystems.com.br>
2024-04-10 11:27:26 -0300

    [s3-lite-installer-2.7.0] - Correção openApplication

--- src/main.js
+++ src/main.js
@@ -69,15 +69,16 @@ function updaterListeners() {
       log.info(`Update disponível: V${info.version}`);
       autoUpdater.downloadUpdate();
     }
-
-    openApplication();
   });
 
   autoUpdater.on("update-not-available", (info) => {
     log.info('Update not Available');
-    log.info(`Alterando para disponível para download`);
-    updateJson.updatedownloaded = 0;
-    writeJson(updateJson);
+    if (updateJson.updatedownloaded === 1) {
+      log.info(`Alterando para disponível para download`);
+      updateJson.updatedownloaded = 0;
+      writeJson(updateJson);
+      openApplication();
+    }
   });
 
   autoUpdater.on("update-downloaded", () => {
@@ -138,5 +139,7 @@ app.whenReady().then(async () => {
     }
   }
 
-  openApplication();
+  if (updateJson.updatedownloaded === 0) {
+    openApplication();
+  }
 });
