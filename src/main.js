78e0c935bd505e9c7c6e5e4092f33895b8d74ef2
Ézio Paes <ezio.paes@cleversystems.net>
2026-09-14 16:16:19 -0300

    [s3lite-2.16.0] - alpha

--- src/main.js
+++ src/main.js
@@ -294,7 +294,7 @@ app.whenReady().then(async () => {
   autoUpdater.autoInstallOnAppQuit = false;
   autoUpdater.allowDowngrade = true;
   autoUpdater.allowPrerelease = true;
-  autoUpdater.channel = "latest"; // alpha, beta, latest
+  autoUpdater.channel = "alpha"; // alpha, beta, latest
 
   log.info(`Version App: ${app.getVersion()}`);
   log.info(`Channel: ${autoUpdater.channel}`);
